2fb6855a053df43f62a9a2de8a556fc4f4b59a84
Trai Nguyen <tnguyen@at-pac.com>
2019-09-06 11:45:07 -0400

    modify game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
 // This is the first version of Javascript for the Psychic Game
 // ************************************************************
-// Defines the variables
+// GLOBAL VARIABLES
+// ************************************************************
 var validLetter = "abcdefghijklmnopqrstuvwxyz";
 var numWin = 0;
 var numLoss = 0;
 var guessLeft = 10;
 var guessList = [];
 
+// FUNCTIONS
+// ************************************************************
 // function to start new game without resetting all scores
 function nextGame () {
   guessLeft = 10;
@@ -36,8 +39,6 @@ function isLetterValid(letterStr) {
   } 
 }
 
-
-
 // This function checks if the guessed letter is valid using isLetterValid() and appends it to the guessList to display
 function submitLetter() {
   
@@ -46,6 +47,8 @@ function submitLetter() {
 
   if (isLetterValid(letterStr) && !(guessList.includes(letterStr))) {
       guessList.push(letterStr);
+      guessLeft--;
+      document.getElementById("guess-left").innerHTML = guessLeft;  
       
   } else {
       alert("The letter selected is either invalid or already existed in the guessed list");
@@ -56,36 +59,31 @@ function submitLetter() {
     
   
   // if the letterStr is the same as computerGuess then return numWin +=1
-  // else letterStr is not the same as computerGuess then guessLeft -= 1
-  if (guessLeft > 0) {
-    if (letterStr === computerLetter) {
-      numWin++;
-      nextGame();
-      updateResults(); 
-    } else {
-      guessLeft--;
-      document.getElementById("guess-left").innerHTML = guessLeft;  
-      updateResults();
-      /////////////////////At the last guessLeft and the letterStr is still not equal to computerLetter, the numLoss is supposed to display +1./////////////////////
-    }
+  if (letterStr === computerLetter) {
+    numWin++;
+    nextGame();
+    updateResults(); 
+    alert(`You won! Correct letter is ${computerLetter}`);
   // when guessLeft = 0 return numLoss and reset the values
   } else if (guessLeft === 0) {
-    numLoss++;
-    document.getElementById("num-loss").innerHTML = numLoss;  
+    numLoss++; 
     nextGame();
-    updateResults() 
+    updateResults();
+    alert(`You lost! Correct letter is ${computerLetter}`);
   }
-  console.log("numWin", numWin);
-  console.log("numLoss", numLoss);
-  console.log("guessLeft", guessLeft);
-  console.log("computerLetter", computerLetter);
+
+  // testing & debugging
+  // console.log("numWin", numWin);
+  // console.log("numLoss", numLoss);
+  // console.log("guessLeft", guessLeft);
+  // console.log("computerLetter", computerLetter);
 } 
 
 // This function reset the variables and restart the game
 function resetGame() {
   var numWin = 0;
   var numLoss = 0;
-  var guessLeft = 0;
+  var guessLeft = 10;
   var guessList = [];
   document.getElementById("num-win").innerHTML = numWin;
   document.getElementById("num-loss").innerHTML = numLoss;
